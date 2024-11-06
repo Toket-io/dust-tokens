d82f206c1ff265636025248a85a32855134f483d
Augusto Collerone <collerone.augusto@gmail.com>
2024-11-06 17:34:33 -0300

    Dependencies

---
+++ contracts/contracts/ERC20Mock.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract ERC20Mock is ERC20, Ownable {
+    uint8 private _decimals;
+
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint8 decimals_,
+        address initialOwner
+    ) ERC20(name, symbol) Ownable(initialOwner) {
+        _decimals = decimals_;
+    }
+
+    function decimals() public view virtual override returns (uint8) {
+        return _decimals;
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
